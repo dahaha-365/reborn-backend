58364f0d88b211b0f869128bf3d8236cdf82bce2
Bolo Lai <cantonbolo@gmail.com>
2026-01-16 15:57:21 +0800

    使用@nestjs/config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', './src/generated/**'],
+    ignores: ['eslint.config.mjs', './src/generated/**', '.claude/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
